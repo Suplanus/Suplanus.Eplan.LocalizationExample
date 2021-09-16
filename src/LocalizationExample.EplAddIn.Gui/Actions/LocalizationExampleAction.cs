f3c97bb353828ce88f54c995e7f47f8e71f2223e
Johann Weiher <info@suplanus.de>
2021-09-16 07:24:09 +0200

    Refactor

--- src/LocalizationExample.EplAddIn.Gui/Actions/LocalizationExampleAction.cs
+++ src/LocalizationExample.EplAddIn.Gui/Actions/LocalizationExampleAction.cs
@@ -1,10 +1,5 @@
 ﻿using Eplan.EplApi.ApplicationFramework;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LocalizationExample.EplAddIn.Gui.Views;
 
 namespace LocalizationExample.EplAddIn.Gui.Actions
